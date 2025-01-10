01e53078b2794e92087b5d497466836342a45e8c
Vsevolod Burtyk <newemailaddress999999@gmail.com>
2025-01-10 15:08:33 +0200

    Initial Commit

---
+++ Twitter Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Twitter Clone
+//
+//  Created by Всеволод Буртик on 10.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
